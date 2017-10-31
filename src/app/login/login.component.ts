7a9af2179ff60b23249844aba23237cdbfc64607
TADSON <stivynho@live.fr>
2017-10-31 21:13:12 +0100

    login ok + suppression de jquery

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ErrorsService } from '../Services/errors/errors.service';
 import { Router } from '@angular/router';
-import * as $ from 'jquery';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'login',
@@ -9,49 +9,52 @@ import * as $ from 'jquery';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  public mail: string;
+  public password: string;
+  public isFetching: boolean;
+  public formIsOk: boolean;
 
-  
-  public mail:string;
-  public password:string;
-
-  constructor(private router: Router) { }
+  constructor(private router: Router, private http: HttpClient) { }
 
   ngOnInit() {
+    this.isFetching = false;
+    this.formIsOk = true;
   }
 
-  login(){
-    $("#connectButton").removeClass("btn-info btn-danger");
-    $("#connectButton").addClass("btn-warning");
-    $("#connectButton").html('<i class="fa fa-circle-o-notch fa-spin"></i> Connexion');
-
-    if(this.inputOk()){
-      //authentification : todo
-      //Redirection avec timeout pour faire sevi
-      setTimeout(() => {
-        this.router.navigate(['/dashboard']);
-      }, 1500);  //5s
-      
-    }
-    else{
-      $("#connectButton").removeClass("btn-warning");
-      $("#connectButton").addClass("btn-danger");
-      $("#connectButton").html('Connexion');
+  login(): void {
+    this.isFetching = true;
+    if (this.inputOk()) {
+      // authentification : todo
+      // Redirection avec timeout pour faire sevi
+      const body = { username: this.mail, password: this.password};
+      this.http
+      .post('http://localhost:1339/login', body)
+      .subscribe((response) => {
+        console.log(response);
+        setTimeout(() => {
+          this.router.navigate(['/dashboard']);
+        }, 1500);  // 5s
+      });
+      } else {
+        this.isFetching = false;
+        this.formIsOk = false;
     }
   }
 
-  inputOk():boolean{
+  inputOk(): boolean {
     let ok = true;
-    let regexMail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    let regexCode = /^[0-9]{8}$/;
+    // tslint:disable-next-line:max-line-length
+    const regexMail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const regexCode = /^[0-9]{8}$/;
     ErrorsService.clearErrorsOnHTML();
-    if(!regexMail.test(this.mail)){
+    if (!regexMail.test(this.mail)) {
       ok = false;
-      ErrorsService.addErrorOnHTML("Renseignez une adresse mail correct");
+      ErrorsService.addErrorOnHTML('Renseignez une adresse mail correct');
     }
-    if(!regexCode.test(this.password)){
+    if (!regexCode.test(this.password)) {
       ok = false;
-      ErrorsService.addErrorOnHTML("Votre code secret doit être strictement composé de 8 chiffres");
-    } 
+      ErrorsService.addErrorOnHTML('Votre code secret doit être strictement composé de 8 chiffres');
+    }
     return ok;
   }
 
